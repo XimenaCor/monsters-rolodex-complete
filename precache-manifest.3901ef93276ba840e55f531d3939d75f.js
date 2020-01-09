c7d3fdc73fb47c23e62772d1db56d41aeb6bae9c
ximena.cordero <ximena.cordero@nearshorecode.com>
2020-01-09 11:07:47 -0400

    Updates

---
+++ precache-manifest.3901ef93276ba840e55f531d3939d75f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc5d89ace71476aa2424ebae8001ebb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "888323ce89ec189d5c51",
+    "url": "/monsters-rolodex/static/css/main.b635b742.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "888323ce89ec189d5c51",
+    "url": "/monsters-rolodex/static/js/main.7ae9c5eb.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
